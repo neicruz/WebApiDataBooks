cb5c3acf272433f7188349f6c31fbd96d2395b36
neicruz <nei.cruz@hotmail.com>
2022-03-24 06:05:01 -0300

    ApiWeb

---
+++ ApiRestWeb/Controllers/LivrosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApiRestWeb.Models;
+using ApiRestWeb.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestWeb.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivrosController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_livroRepository.Listar());
+        }
+    }
+}
---
+++ ApiRestWeb/Repositories/LivroRepository.cs
@@ -0,0 +1,25 @@
+﻿using ApiRestWeb.Models;
+using ApiRestWeb.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestWeb.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+
+        public LivroRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+        public List<Livro> Listar()
+        {
+
+            return _context.Livros.ToList();
+        }
+    }
+
+}
